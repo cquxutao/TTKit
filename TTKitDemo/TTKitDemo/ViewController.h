09616ebd581e4e74756fc0014ba6bd2089d85e00
cquxutao <178052394@qq.com>
2017-11-02 23:20:24 +0800

    add Demo

---
+++ TTKitDemo/TTKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TTKitDemo
+//
+//  Created by 徐涛 on 02/11/2017.
+//  Copyright © 2017 徐涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TTKitDemo/TTKitDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TTKitDemo
+//
+//  Created by 徐涛 on 02/11/2017.
+//  Copyright © 2017 徐涛. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+@end
